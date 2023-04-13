8cef967ab9ea1ad5131b33ae32fe5e6f3b5e4948
Morgane Mahé <maeamahe@gmail.com>
2023-04-13 21:16:39 +0200

    #4: add page galerie.js et galerie.html

---
+++ galerie.js
@@ -0,0 +1,10 @@
+/* 
+1. Récupérer les éléments à filter et les mettre dans les constantes
+*/
+const onglet = document.querySelectorAll("#");
+
+/*
+2. Quand l'onglet est cliqué(addEventListenner), il devient actif, 
+rajouter la galerie correspondante,
+retirer les autres
+*/
