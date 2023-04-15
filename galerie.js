663b0b5d12f14cc3fe86451a86ef605973766ff1
Morgane Mahé <maeamahe@gmail.com>
2023-04-15 09:54:31 +0200

    #9: add debut algo for image gallery

--- galerie.js
+++ galerie.js
@@ -1,10 +1,20 @@
 /* 
 1. Récupérer les éléments à filter et les mettre dans les constantes
 */
-const onglet = document.querySelectorAll("#");
+console.log('hello world')
+const categories = document.querySelectorAll('#categories button');
+console.log(categories)
+// récupération d'un tableau de longueur 7
+
+const images = document.querySelectorAll('#galery img')
+console.log(images)
+// récupération d'un tableau de valeur 6
 
 /*
-2. Quand l'onglet est cliqué(addEventListenner), il devient actif, 
+2. Quand le button est cliqué(addEventListenner), 
+il devient actif, 
 rajouter la galerie correspondante,
 retirer les autres
+tableau imbriqué 1er avec category id à récupérer 
+2ème tableau avec les images
 */
