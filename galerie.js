06b5d58380f1e01fac1ff7205f6d570414b3c3e6
Morgane Mahé <maeamahe@gmail.com>
2023-05-10 20:42:49 +0200

    #12: work on galerie.js

--- galerie.js
+++ galerie.js
@@ -6,14 +6,24 @@ const onglets = document.querySelectorAll('.category');
 console.log(onglets)
 // récupération d'un tableau de longueur 7
 
+// initialiser un nombre à 0 que l'on va comparer à l'onglet clique via un data
+// car sinon ça ne déascative pas les autres
+
+let number = 0;
 onglets.forEach((onglet) => {
     onglet.addEventListener("click",()=>{
     console.log("clicked")
         if (onglet.classList.contains("active")) {
-            onglet.classList.remove("active");
+    //        onglet.classList.remove("active");
         } else {
             onglet.classList.add("active");
         }
+    number = onglet.getAttribute('data-btn');
+    for (i=0; i< onglets.length; i++){
+        if (onglets[i].getAttribute('data-btn') != number) {
+            onglets[i].classList.remove("active")
+        }
+    }
     })
 });
 
