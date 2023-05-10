0fd8c247fc66d975b2a3cb90a1e28f6ddbe7f578
Morgane Mahé <maeamahe@gmail.com>
2023-05-10 20:10:30 +0200

    #11: work on galerie.js

--- galerie.js
+++ galerie.js
@@ -2,11 +2,22 @@
 1. Récupérer les éléments à filter et les mettre dans les constantes
 */
 console.log('hello world')
-const onglets = document.querySelectorAll('#category');
+const onglets = document.querySelectorAll('.category');
 console.log(onglets)
 // récupération d'un tableau de longueur 7
 
-const images = document.querySelectorAll('#galery img')
+onglets.forEach((onglet) => {
+    onglet.addEventListener("click",()=>{
+    console.log("clicked")
+        if (onglet.classList.contains("active")) {
+            onglet.classList.remove("active");
+        } else {
+            onglet.classList.add("active");
+        }
+    })
+});
+
+const images = document.querySelectorAll('.galery__image')
 console.log(images)
 // récupération d'un tableau de valeur 6
 
@@ -21,12 +32,3 @@ tableau imbriqué 1er avec category id à récupérer
 switch case?
 */
 
-
-categories.forEach((category) => {
-    category.addEventListener("click", () => {
-        if (category.classList.contains("active")) {
-            return;
-        } else {
-            category.classList.add("active");
-    }}
-    )
