d9f6c322d85100a45b2a5b273ee0818d7774cf81
Morgane Mahé <maeamahe@gmail.com>
2023-11-26 14:29:33 +0100

    retrait console.log

--- galerie.js
+++ galerie.js
@@ -4,18 +4,15 @@ document.addEventListener("DOMContentLoaded", function() {
     les éléments à filter et les mettre dans les constantes
     */
     const onglets = document.querySelectorAll('.category');
-    console.log(onglets)
     // récupération d'un tableau de longueur 7
 
     const images = document.querySelectorAll('.galery__image')
-    console.log(images)
     // récupération d'un tableau de valeur 6
 
     const noms = ['tout','mariage', 'bebe', 'grossesse', 'famille', 'couple', 'bapteme'];
     
     onglets.forEach((onglet) => {
     onglet.addEventListener("click",()=>{
-    console.log("clicked")
         if (onglet.classList.contains("active")) {
     //        onglet.classList.remove("active");
         } else {
@@ -25,7 +22,6 @@ document.addEventListener("DOMContentLoaded", function() {
         // initialiser un nombre à 0 que l'on va comparer à l'onglet clique via un data
         // car sinon ça ne déascative pas les autres
         let number = onglet.getAttribute('data-btn');
-        console.log(`valeur number: ${number}`)
         for (i=0; i< onglets.length; i++){
             if (onglets[i].getAttribute('data-btn') != number) {
                 onglets[i].classList.remove("active")
@@ -36,10 +32,8 @@ document.addEventListener("DOMContentLoaded", function() {
     // si autre que 0, mettre par catégorie miage...
             for (j = 0 ; j < noms.length ; j++) {
                 if (j == number) {
-                    console.log(`valeur de :${j}`)
                     images.forEach((image) => {
                         if (image.classList.contains(noms[j]) || j === 0) {
-                            console.log(`noms index:${noms[j]}`)
                             image.classList.add("active");
                     } else {
                             image.classList.remove("active");
